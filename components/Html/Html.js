083442b62ef1fb07367d13ca09af15d81eda392f
Takuya Matsuyama <nora@odoruinu.net>
2017-06-10 10:40:55 +0900

    Add opengraph

--- components/Html/Html.js
+++ components/Html/Html.js
@@ -19,6 +19,20 @@ function Html({ title, description, body, debug }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="Takuya Matsuyama" />
         <meta name="author" content="craftzdog" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@craftzdog" />
+        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:title" content={title || config.title} />
+        <meta name="twitter:description" content={description || config.description} />
+        <meta name="twitter:image" content="/cover.jpg" />
+
+        <meta property="og:site_name" content="Takuya Matsuyama's Homepage" />
+        <meta property="og:title" content={title || config.title} />
+        <meta property="og:description" content={description || config.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/cover.jpg" />
+
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700|Lato:300,400" />
