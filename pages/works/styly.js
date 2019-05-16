1cb5189ec2020168f08929424bc84382b7ae9869
Takuya Matsuyama <hi@craftz.dog>
2019-05-16 11:38:18 +0900

    feat(works): Add pictures for styly

--- pages/works/styly.js
+++ pages/works/styly.js
@@ -23,6 +23,14 @@ export default class WorkPage extends Component {
           <img src="/works/styly_01.png" className="image-screenshot" />
         </div>
 
+        <div>
+          <img src="/works/styly_02.png" className="image-screenshot" />
+        </div>
+
+        <div>
+          <img src="/works/styly_03.png" className="image-screenshot" />
+        </div>
+
         <h3>STYLY (2017, Psychic VR Lab)</h3>
         <div className="work-description">
           <div>ファッションブランドのためのVRツール</div>
@@ -36,6 +44,7 @@ export default class WorkPage extends Component {
           </ul>
         </div>
         <div className="work-long-description">
+          <p>主にユーザ認証・アセット選択などのUIと、バックエンドを担当。</p>
           <p>
             This is a VR tool for building virtual space towards extending
             fashion brand&apos;s expression ability of their concept. I
